8efde09d7f8566a22b8262ced5c9deae20c3d298
khumaryn <119412655+khumaryn@users.noreply.github.com>
2022-12-13 02:12:26 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml;
+
+namespace classtask4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // verilmis ededin cut olub olmadigini yoxlaya
+            Console.WriteLine("ededi daxil et");
+            int num=Convert.ToInt32(Console.ReadLine());
+            
+           
+        }
+        static bool cuttek(int num)
+        {
+            return (num % 2 == 0);
+               
+            
+           
+               
+      
+        }
+    }
+}
